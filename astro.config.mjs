799cf243fae4d8f6907e39777f982b5dfddd2b11
SaveMP3 Developer <developer@savemp3.org>
2026-01-03 18:12:23 +0300

    Google Analytics (gtag) kodunu tüm sayfalarına eklendi

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	trailingSlash: 'never',
-	build: {
-		format: 'directory'
-	}
+	trailingSlash: 'ignore'
 });
