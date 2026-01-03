f055a66e9db14e8df2032ee2cb62f5730b783e42
SaveMP3 Developer <developer@savemp3.org>
2026-01-03 16:17:46 +0300

    Trailing slash yapılandırması güncellendi

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	// Static build - serve public files as-is
 	output: 'static',
 	trailingSlash: 'never',
+	build: {
+		format: 'directory'
+	}
 });
